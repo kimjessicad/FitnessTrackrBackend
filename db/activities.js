26c4cef60cdaccb835a51a60ea2d13bd060546a5
Jessica <kimjessicad@gmail.com>
2022-07-26 10:47:13 -0500

    finished db/activities

--- db/activities.js
+++ db/activities.js
@@ -1,79 +1,112 @@
+/* eslint-disable no-useless-catch */
 const client = require("./client");
 
 // database functions
 async function createActivity({ name, description }) {
-    const { rows: [ activity ] } = await client.query(`
+  try {
+    const {
+      rows: [activity]
+    } = await client.query(
+      `
       INSERT INTO activities(name, description) 
       VALUES($1, $2)
       RETURNING *;
-    `, [name, description]);
+    `,
+      [name, description]
+    );
 
     return activity
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getAllActivities() {
-  const { rows: activityIds } = await client.query(`
-  SELECT id
-  FROM activities;
+  try {
+    const {
+      rows
+    } = await client.query(
+      `
+      SELECT *
+      FROM activities;
 `);
-
-const activities = await Promise.all(activityIds.map(
-  activity => getActivityById( activity.id )
-));
-
-return activities;
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getActivityById(id) {
-  const { rows: [ activity ]  } = await client.query(`
+  try {
+
+    const {
+      rows: [activity]
+    } = await client.query(`
   SELECT *
   FROM activities
-  WHERE id=$1;
-`, [id]);
+  WHERE id=${id};
+`,);
 
-if (!activity) {
-  throw {
-    name: "ActivityNotFoundError",
-    message: "Could not find an activity with that id"
-  };
-}
-
+    return activity;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getActivityByName(name) {
-  const { rows: [activity] } = await client.query(`
+  try {
+    const {
+      rows: [activity]
+    } = await client.query(`
     SELECT *
-    FROM activity
+    FROM activities
     WHERE name=$1;
   `, [name]);
 
-return activity;}
-
-async function attachActivitiesToRoutines(routines) {
-  
+    return activity;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function updateActivity({ id, ...fields}) {
-  // don't try to update the id
-  // do update the name and description
-  // return the updated activity
+async function attachActivitiesToRoutines(routines) {
+  // try {
+  //   const createPostTagPromises = tagList.map(
+  //     tag => createPostTag(postId, tag.id)
+  //   );
+
+  //   await Promise.all(createPostTagPromises);
+      
+  //   return await getPostById(postId);
+  // } catch (error) {
+  //   throw error;
+  // }
+}
+
+async function updateActivity({ id, ...fields }) {
   const setString = Object.keys(fields).map(
-    (key, index) => `"${ key }"=$${ index + 1 }`
+    (key, index) => `"${key}"=$${index + 1}`
   ).join(', ');
 
-  // return early if this is called without fields
   if (setString.length === 0) {
     return;
   }
 
-  const result = await client.query(`
-  UPDATE activity
-  SET ${ setString }
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(`
+  UPDATE activities
+  SET ${setString}
   WHERE id=${id}
   RETURNING *;
-`, Object.values(fields));
+`, Object.values(fields)
+    );
 
-return result;
+    return activity;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
